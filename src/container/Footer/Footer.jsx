b42edc65cddf22348fdd3c2d248c2f6e9cd8ef8f
Mabast <mabast.niga@yahoo.com>
2022-05-16 22:02:25 -0500

    created footer columns

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
+import { images } from "../../constants";
 import { FooterOverlay, Newsletter } from "../../components";
 
 import "./Footer.css";
@@ -9,6 +10,39 @@ const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">address here</p>
+        <p className="p__opensans">phone no</p>
+        <p className="p__opensans">phone 2</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer logo" />
+        <p className="p__opensans">quote here</p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: "2rem" }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiInstagram />
+          <FiTwitter />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">time here</p>
+        <p className="p__opensans">Saturday-Sunday</p>
+        <p className="p__opensans">time here</p>
+      </div>
+    </div>
+    <div className="footer_copyright">
+      <p className="p__opensans">2021 Gericht. All rights reserved</p>
+    </div>
   </div>
 );
 
